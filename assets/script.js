a56e9ffa0c7b040cd499b3be75a98b43c7a96fd0
Michael Crandall <mec192@gmail.com>
2022-09-12 22:23:53 -0500

    added the question fields in the java

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,48 @@
 var startBtn = document.getElementById('start-btn');
 var nextBtn = document.getElementById('nxt-btn');
 var questionContEl = document.getElementById('questionBox')
+var questionEl = document.getElementById('question')
+var answerBtnsEl = document.getElementById('answer-buttons')
+
+var score = 0;
+
+var questions = [
+    {
+        question:
+        answers: [
+            {text: , correct:true },
+            {text: , correct:false },
+            {text: , correct:false },
+            {text: , correct:false },
+        ]
+    },
+    {
+        question:
+        answers: [
+            {text: , correct:true },
+            {text: , correct:false },
+            {text: , correct:false },
+            {text: , correct:false },
+        ]
+
+    },
+    {
+        question:
+        answers: [
+            {text: , correct:true },
+            {text: , correct:false },
+            {text: , correct:false },
+            {text: , correct:false },
+        ]
+
+    },
+    {
+        question:
+        answers: [
+            {text: , correct:true },
+            {text: , correct:false },
+            {text: , correct:false },
+            {text: , correct:false },
+        ]
+    }
+]
